aa83cb58a71ceeeb9a9fe09514d19de7df5e0e49
Aman Kumar <kumaraman140795@gmail.com>
2025-01-15 13:28:01 +0530

    Create a basic migration runner using jscodeshift API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('Just checking!!!');
+const path = require('path');
+const { run: jscodeshift } = require('jscodeshift/src/Runner');
+
+const transformPath = path.join(__dirname, 'transform.js');
+
+// node src/index.js path-to-migrate
+const args = process.argv.slice(2);
+
+if(args.length === 0) {
+    console.error('Please provide a path for migration');
+    process.exit(1);
+}
+
+const sourcePath = args[0];
+
+async function main() {
+    try {
+        const result = await jscodeshift(
+            transformPath,
+            [sourcePath],
+            {
+                dry: false,
+                verbose: 2,
+                print: false,
+                babel: true,
+                extensions: 'js',
+                parser: 'flow'
+            }
+        )
+        
+        console.log(`Migration completed: ${result}`);
+    }
+    catch(error) {
+        console.error(`Migration failed: ${error}`);
+    }
+}
+
+main();
